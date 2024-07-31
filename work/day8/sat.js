ae4c6389fe491db77329602897e16cb8af014121
Satya850 <satyadoppalapudi143@gmail.com>
2024-07-31 09:52:27 +0530

    Add files via upload

---
+++ work/day8/sat.js
@@ -0,0 +1,36 @@
+function formValidation()
+{
+var uemail = document.registration.uemail;
+var passid = document.registration.passid;
+if(userid_validation(uid,5,12))
+{
+if(ValidateEmail(uemail))
+{
+}
+}
+return false
+}
+function ValidateEmail(uemail)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(uemail.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+uemail.focus();
+return false;
+}
+function passid_validation(passid,mx,my)
+{
+var passid_len = passid.value.length;
+if (passid_len == 0 ||passid_len >= my || passid_len < mx)
+{
+alert("Password should not be empty / length be between "+mx+" to "+my);
+passid.focus();
+return false;
+}
+return true;
+}
